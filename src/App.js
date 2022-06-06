eaf0c2ce743efc7052c83cb1431ead3986a30acb
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-06 17:48:19 +0600

    bootstrap setup done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 function App() {
   return (
