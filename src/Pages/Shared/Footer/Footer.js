cff9ab73c35e049cf67d49da78fe9a13e9585e5a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-06 21:30:09 +0600

    alhamdulillah task 2 done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -20,22 +20,22 @@ const Footer = () => {
                 <h6 class="text-uppercase fw-bold mb-4">Products</h6>
                 <p>
                   <a href="#!" class="text-reset">
-                    Angular
+                    slow issues
                   </a>
                 </p>
                 <p>
                   <a href="#!" class="text-reset">
-                    React
+                    SLA’s and warranties
                   </a>
                 </p>
                 <p>
                   <a href="#!" class="text-reset">
-                    Vue
+                    incoming calls
                   </a>
                 </p>
                 <p>
                   <a href="#!" class="text-reset">
-                    Laravel
+                    headache
                   </a>
                 </p>
               </div>
@@ -75,7 +75,7 @@ const Footer = () => {
           </div>
         </section>
         <hr />
-        <div class="text-center p-4">© 2021 Copyright: ShayemHossain</div>
+        <div class="text-center p-4">© 2022 Copyright: ShayemHossain</div>
       </footer>
     </div>
   );
