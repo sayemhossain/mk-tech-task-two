90224e82dbaf6e52a0539e7ffedaa4a03dbdbe00
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-06 18:26:38 +0600

    banner section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
+import Home from "./Pages/Home/Home/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Home></Home>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,63 @@
+import React from "react";
+import banner1 from "../../../img/banner/banner1.webp";
+import banner4 from "../../../img/banner/banner4.jpg";
+import banner3 from "../../../img/banner/banner3.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <div
+        id="carouselExampleControls"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src={banner4}
+              style={{ maxHeight: "100vh" }}
+              class="d-block w-100 img-fluid "
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src={banner3}
+              class="d-block w-100 img-fluid"
+              style={{ maxHeight: "100vh" }}
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src={banner1}
+              class="d-block w-100 img-fluid"
+              style={{ maxHeight: "100vh" }}
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
